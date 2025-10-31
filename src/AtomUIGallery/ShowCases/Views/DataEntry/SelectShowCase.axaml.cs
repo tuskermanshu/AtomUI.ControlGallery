a774607effa3518c410898f8cc089e377c6fc43e
chinboy <qinware@163.com>
2025-10-31 08:45:06 +0800

    Add Custom Search for Select

--- src/AtomUIGallery/ShowCases/Views/DataEntry/SelectShowCase.axaml.cs
+++ src/AtomUIGallery/ShowCases/Views/DataEntry/SelectShowCase.axaml.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using AtomUIGallery.ShowCases.ViewModels;
 using Avalonia.ReactiveUI;
 using ReactiveUI;
@@ -10,5 +11,19 @@ public partial class SelectShowCase : ReactiveUserControl<SelectViewModel>
     {
         this.WhenActivated(disposables => { });
         InitializeComponent();
+        CustomSearchSelect.FilterFn = CustomFilter;
+    }
+
+    public static bool CustomFilter(object value, object filterValue)
+    {
+        // 使用大小写敏感的搜索
+        var valueStr = value.ToString();
+        Debug.Assert(valueStr != null);
+        var filterStr = filterValue.ToString();
+        if (filterStr == null)
+        {
+            return false;
+        }
+        return valueStr.Contains(filterStr, StringComparison.Ordinal);
     }
 }
